e16c761accdfb819923e7b577f5a27b3feb280fb
Alex Wilson <nikosai@gmail.com>
2011-01-03 22:25:14 +1000

    initial cm revision

---
+++ cm.pro
@@ -0,0 +1,4 @@
+QT = dbus network core
+
+SOURCES += main.cpp manager.cpp service.cpp
+HEADERS += manager.h service.h
---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <QtDBus>
+#include <QCoreApplication>
+
+#include "manager.h"
+#include "service.h"
+
+#include <iostream>
+#include <QMetaType>
+
+void printService(const QString &path)
+{
+	NetConnmanServiceInterface svc("net.connman", path, QDBusConnection::systemBus());
+
+	QDBusPendingReply<QVariantMap> repl = svc.GetProperties();
+	repl.waitForFinished();
+	QVariantMap props = repl.value();
+	
+	QString name = argVarConvert<QString>(props.value("Name"));
+	
+	std::cout << qPrintable(name) << "\t" << qPrintable(path) << "\n";
+	QString type = argVarConvert<QString>(props.value("Type"));
+	QString state = argVarConvert<QString>(props.value("State"));
+	bool autoConnect = argVarConvert<bool>(props.value("AutoConnect"));
+	bool fav = argVarConvert<bool>(props.value("Favorite"));
+	std::cout << "\ttype:" << qPrintable(type) << " state:" << qPrintable(state);
+	if (autoConnect)
+		std::cout << " auto";
+	if (fav)
+		std::cout << " fav";
+	std::cout << "\n";
+	
+	if (props.keys().contains("IPv4")) {
+		QVariantMap ipv4 = argVarConvert<QVariantMap>(props.value("IPv4"));
+		QString method = argVarConvert<QString>(ipv4.value("Method"));
+		QString address = argVarConvert<QString>(ipv4.value("Address"));
+		QString netmask = argVarConvert<QString>(ipv4.value("Netmask"));
+		QString gateway = argVarConvert<QString>(ipv4.value("Gateway"));
+		
+		std::cout << "\tipv4 method:" << qPrintable(method) << " addr:" << qPrintable(address)
+			<< " mask:" << qPrintable(netmask) << " gw:" << qPrintable(gateway) << "\n";
+	}
+	
+	if (props.keys().contains("IPv6")) {
+		QVariantMap ipv6 = argVarConvert<QVariantMap>(props.value("IPv6"));
+	}
+	
+	if (props.keys().contains("Nameservers")) {
+		QStringList ns = argVarConvert<QStringList>(props.value("Nameservers"));
+		std::cout << "\tns: " << qPrintable(ns.join(" ")) << "\n";
+	}
+	
+	if (props.keys().contains("Domains")) {
+		QStringList ns = argVarConvert<QStringList>(props.value("Domains"));
+		std::cout << "\tdomains: " << qPrintable(ns.join(" ")) << "\n";
+	}
+	
+	if (type == "wifi") {
+		QString security = argVarConvert<QString>(props.value("Security"));
+		QString passph = argVarConvert<QString>(props.value("Passphrase"));
+		
+		std::cout << "\twifi sec:" << qPrintable(security) << " pass:" << qPrintable(passph) << "\n";
+	}
+	
+	if (type == "ethernet") {
+		QVariantMap eth = argVarConvert<QVariantMap>(props.value("Ethernet"));
+		QString intfn = argVarConvert<QString>(eth.value("Interface"));
+		QString mac = argVarConvert<QString>(eth.value("Address"));
+		quint16 mtu = argVarConvert<quint16>(eth.value("MTU"));
+		std::cout << "\ton:" << qPrintable(intfn) << " mac:" << qPrintable(mac);
+		std::cout << " mtu:" << mtu << "\n";
+	}
+	
+	std::cout << "\n";
+}
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication a(argc, argv);
+	QString intf(argv[1]);
+	
+	NetConnmanManagerInterface cm("net.connman", "/", QDBusConnection::systemBus());
+	if (!cm.isValid()) {
+		std::cout << "Could not connect to connman on D-bus. Is connmand running?\n";
+		return 1;
+	}
+	
+	if (intf == "") {
+		QList<QDBusObjectPath> paths = cm.ServicePaths();
+
+		for (int n = 0; n < paths.size(); ++n) {
+			QDBusObjectPath path = paths.at(n);
+			printService(path.path());
+		}
+	} else {
+		QDBusPendingReply<QDBusObjectPath> repl = cm.LookupService(intf);
+		repl.waitForFinished();
+		if (!repl.isValid()) {
+			std::cerr << "cm: error looking for service: LookupService reply invalid\n";
+			return 1;
+		}
+		
+		QString path = repl.value().path();
+		if (argc == 2) {
+			printService(path);
+		} else {
+			NetConnmanServiceInterface svc("net.connman", path, QDBusConnection::systemBus());
+			
+			QString op(argv[2]);
+			op = op.toLower();
+			
+			if (op == "up" || op == "connect") {
+				QDBusPendingReply<> crep = svc.Connect();
+				crep.waitForFinished();
+				
+				if (crep.isError()) {
+					std::cerr << "cm: error connecting service " << qPrintable(path) << "\n";
+					return 1;
+				}
+			} else if (op == "down" || op == "disconnect") {
+				QDBusPendingReply<> drep = svc.Disconnect();
+				drep.waitForFinished();
+				
+				if (drep.isError()) {
+					std::cerr << "cm: error disconnecting service " << qPrintable(path) << "\n";
+					return 1;
+				}
+			} else if (op == "pass" || op == "pwd" || op == "passphrase") {
+				QString ps(argv[3]);
+				QDBusPendingReply<> rep = svc.SetProperty("Passphrase", QDBusVariant(ps));
+				rep.waitForFinished();
+				
+				if (rep.isError()) {
+					std::cerr << "cm: error setting passphrase on " << qPrintable(path) << "\n";
+					return 1;
+				}
+			} else {
+				std::cerr << "cm: I don't know that operation?\n";
+				return 1;
+			}
+		}
+	}
+
+  return 0;
+}
